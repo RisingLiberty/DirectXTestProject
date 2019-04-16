7a51b633589783b8c2ff4dcfab7f3adfe7a4d049
Nick De Breuck <nick.de.breuck@outlook.com>
2019-04-16 16:32:48 +0200

    adjusted frame resource for lightning

--- DirectXTestProject/DirectXTestProject/src/1.0 Core/FrameResource.h
+++ DirectXTestProject/DirectXTestProject/src/1.0 Core/FrameResource.h
@@ -16,7 +16,7 @@
 struct FrameResource
 {
 public:
-	FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT waveVertCount = 1);
+	FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount = 1, UINT waveVertCount = 1);
 	FrameResource(const FrameResource& other) = delete;
 	FrameResource& operator=(const FrameResource& other) = delete;
 	~FrameResource();
@@ -30,11 +30,16 @@ public:
 	std::unique_ptr<UploadBuffer<PassConstants>> PassCB = nullptr;
 	std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB = nullptr;
 
+	// Since lightning
+	std::unique_ptr<UploadBuffer<MaterialConstants>> MaterialCB = nullptr;
+
 	// Temp value for DrawingD3DAppIII
 	// We cannot update a dynamic vertex buffer untill the GPU is done processing
 	// the commands that reference it. So each frame needs their own
-	std::unique_ptr<UploadBuffer<Vertex>> WavesVB = nullptr;
+	//std::unique_ptr<UploadBuffer<Vertex>> WavesVB = nullptr;
 
+	// Temp value for LightningWaves
+	std::unique_ptr<UploadBuffer<LightningVertex>> WavesVB = nullptr;
 
 	// Frence value to mark commands up to this fence point.
 	// This lets us check if these frame resources are still in use by the GPU.
